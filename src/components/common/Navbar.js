d4bb31a562a88492cf8d7034dfb791664607d454
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-12-12 20:18:23 +0800

    built out common navigation bar component

---
+++ src/components/common/Navbar.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+import { Link } from 'react-router-dom';
+import {
+  Layout,
+  Dropdown as AntDropdown,
+  Button as AntButton,
+  Space,
+  Menu,
+} from 'antd';
+import './Navbar.css';
+import {
+  Menu as MenuIcon,
+  Home,
+  Search,
+  MapPin,
+  User,
+  LogOut as LogoutIcon,
+  BarChart2,
+  Briefcase,
+} from 'react-feather';
+
+const { Header } = Layout;
+
+function Navbar() {
+  const { authService } = useOktaAuth();
+
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <div className="nav-links">
+          <Link to="/">
+            <div>
+              <Home />
+              <p>Home</p>
+            </div>
+          </Link>
+        </div>
+      </Menu.Item>
+      <Menu.Item>
+        <div className="nav-links">
+          <Link to="/trips">
+            <div>
+              <Briefcase />
+              <p>Trips</p>
+            </div>
+          </Link>
+        </div>
+      </Menu.Item>
+      <Menu.Item>
+        <div className="nav-links">
+          <Link to="/example-list">
+            <div>
+              <MapPin />
+              <p>Pins</p>
+            </div>
+          </Link>
+        </div>
+      </Menu.Item>
+      <Menu.Item>
+        <div className="nav-links">
+          <Link to="/datavis">
+            <div>
+              <BarChart2 />
+              <p>Data</p>
+            </div>
+          </Link>
+        </div>
+      </Menu.Item>
+      <Menu.Item>
+        <div className="nav-links">
+          <Link to="/profile-list">
+            <div>
+              <User />
+              <p>Profile</p>
+            </div>
+          </Link>
+        </div>
+      </Menu.Item>
+      <Menu.Item>
+        <div className="nav-links" onClick={() => authService.logout()}>
+          <div>
+            <LogoutIcon />
+            <p>Logout</p>
+          </div>
+        </div>
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Layout>
+      <Header className="header">
+        <h1>Resfeber</h1>
+        {/* <Button
+          handleClick={() => authService.logout()}
+          buttonText="Logout"
+          type="default"
+        /> */}
+        <Space direction="vertical">
+          <Space wrap="true">
+            <AntDropdown overlay={menu} placement="bottomLeft">
+              <AntButton>
+                <MenuIcon />
+              </AntButton>
+            </AntDropdown>
+          </Space>
+        </Space>
+      </Header>
+    </Layout>
+  );
+}
+
+export default Navbar;
