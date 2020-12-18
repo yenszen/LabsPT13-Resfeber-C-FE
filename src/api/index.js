cde0db0b6df9c82f68e9a86d230a4ff400dbf07a
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-12-19 00:15:20 +0800

    added trip API call functions using testing db

--- src/api/index.js
+++ src/api/index.js
@@ -51,16 +51,60 @@ const getProfileData = authState => {
 
 const getTestProfileData = () => {
   try {
-    return axios
-      .get('https://my-json-server.typicode.com/yenszen/database/profile')
-      .then(res => {
-        console.log('test profile data', res.data);
-        return res.data;
-      });
+    return axios.get('http://localhost:3001/profile').then(res => {
+      console.log('test profile data', res.data);
+      return res.data;
+    });
   } catch (error) {
     console.log(error);
     return [];
   }
 };
 
-export { sleep, getExampleData, getProfileData, getDSData, getTestProfileData };
+// ALL TRIPS API CALLS RESIDE BELOW
+const getMyTrips = () => {
+  try {
+    return axios.get('http://localhost:3001/myTrips').then(res => {
+      // console.log('getMyTrips', res.data);
+      return res.data;
+    });
+  } catch (error) {
+    console.log('getMyTrips', error);
+    return [];
+  }
+};
+
+const createNewTrip = data => {
+  try {
+    return axios.post(`http://localhost:3001/myTrips`, data).then(res => {
+      console.log('createNewTrip', res.data);
+    });
+  } catch (error) {
+    console.log('createNewTrip', error);
+    return [];
+  }
+};
+
+const addToTrip = (tripId, data) => {
+  try {
+    return axios
+      .patch(`http://localhost:3001/myTrips/${tripId}`, data)
+      .then(res => {
+        console.log('addToTrip', res.data);
+      });
+  } catch (error) {
+    console.log('addToTrip', error);
+    return [];
+  }
+};
+
+export {
+  sleep,
+  getExampleData,
+  getProfileData,
+  getDSData,
+  getTestProfileData,
+  getMyTrips,
+  createNewTrip,
+  addToTrip,
+};
